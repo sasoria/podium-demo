82f7bed89859aa00c943061396acfc4cb1094902
sasoria <sergio.alberto.arevalo.soria@nav.no>
2020-11-01 21:37:26 +0100

    Add layout server

---
+++ src/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const Layout = require('@podium/layout');
+
+const app = express();
+
+const layout = new Layout({
+  name: 'Layout',
+  pathname: '/demo',
+});
+
+const podlet = layout.client.register({
+  name: 'podlet',
+  uri: 'http://localhost:7100/manifest.json',
+});
+
+app.use(layout.middleware());
+
+app.get('/demo', async (req, res) => {
+  const incoming = res.locals.podium;
+  const response = await podlet.fetch(incoming);
+
+  incoming.view.title = 'My Super Page';
+
+  res.podiumSend(
+    `<div>
+       ${response}
+    </div>`
+  );
+});
+
+app.listen(7000);
