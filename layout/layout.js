27713e74f5008c1bdf97e78d951a518dbb036720
sasoria <sergio.alberto.arevalo.soria@nav.no>
2021-04-11 19:38:53 +0200

    Use incoming podlets

--- layout/layout.js
+++ layout/layout.js
@@ -35,7 +35,6 @@ app.use(layout.middleware());
 
 app.get(layout.pathname(), async (req, res, next) => {
   const incoming = res.locals.podium;
-  incoming.view.title = "Podium Layout";
 
   const podlets = await Promise.all([
     podletA.fetch(incoming),
@@ -43,10 +42,8 @@ app.get(layout.pathname(), async (req, res, next) => {
     podletC.fetch(incoming),
   ]);
 
-  podlets.map((podlet) => {
-    layout.css(podlet.css);
-    layout.js(podlet.js);
-  });
+  incoming.view.title = "Podium Layout";
+  incoming.podlets = podlets;
 
   res.podiumSend(
     `<section>
