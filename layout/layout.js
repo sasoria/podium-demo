f891cda53e12d586f20c3b8976a83498ce395cae
sasoria <sergio.alberto.arevalo.soria@nav.no>
2021-07-18 21:57:06 +0200

    Add more routes

--- layout/layout.js
+++ layout/layout.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const Layout = require("@podium/layout");
+const utils = require("@podium/utils");
 
 const app = express();
 
@@ -33,6 +34,22 @@ const podletC = layout.client.register({
 
 app.use(layout.middleware());
 
+layout.view(
+  (incoming, body, head) => `<!doctype html>
+<html lang="${incoming.context.locale}">
+    <head>
+        <meta charset="${incoming.view.encoding}">
+        ${incoming.css.map(utils.buildLinkElement).join("\n")}
+        ${incoming.js.map(utils.buildScriptElement).join("\n")}
+        <title>${incoming.view.title}</title>
+        ${head}
+    </head>
+    <body>
+        ${body}
+    </body>
+</html>`
+);
+
 app.get(layout.pathname(), async (req, res, next) => {
   const incoming = res.locals.podium;
 
@@ -45,13 +62,47 @@ app.get(layout.pathname(), async (req, res, next) => {
   incoming.view.title = "Podium Layout";
   incoming.podlets = podlets;
 
-  res.podiumSend(
-    `<section>
-       ${podlets[0].content}
-       ${podlets[1].content}
-       ${podlets[2].content}
-    </section>`
-  );
+  const head = `
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  `;
+
+  const body = `
+  <section>
+    ${podlets[0].content}
+    ${podlets[1].content}
+    ${podlets[2].content}
+  </section>
+`;
+  const document = layout.render(incoming, body, head);
+  res.send(document);
+});
+
+app.get(`${layout.pathname()}page`, async (req, res, next) => {
+  const incoming = res.locals.podium;
+
+  const podlets = await Promise.all([
+    podletA.fetch(incoming),
+    podletB.fetch(incoming),
+  ]);
+
+  incoming.view.title = "Podium Layout";
+  incoming.podlets = podlets;
+
+  const head = `
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  `;
+
+  const body = `
+  <section>
+    ${podlets[0].content}
+    ${podlets[1].content}
+  </section>
+`;
+
+  const document = layout.render(incoming, body, head);
+  res.send(document);
 });
 
 app.listen(7000, () => console.log("Layout listening on port 7000"));
