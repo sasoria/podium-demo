5e7fc3ba140fe09591b8f5462b52e63e1ac56cf0
sasoria <sergio.alberto.arevalo.soria@nav.no>
2021-07-19 12:26:08 +0200

    Add more pages

--- layout/layout.js
+++ layout/layout.js
@@ -8,12 +8,18 @@ const app = express();
 const { layout, podletA, podletB, podletC } = setupLayout();
 
 const homePodlets = [podletA, podletB, podletC];
-const pagePodlets = [podletA, podletB];
+const firstPodlets = [podletA, podletB];
+const secondPodlets = [podletA, podletC];
+const thirdPodlets = [podletB, podletC];
 
 app.use(layout.middleware());
 
 app.get(layout.pathname(), handleHome(layout, homePodlets));
 
-app.get(`${layout.pathname()}page`, handlePage(layout, pagePodlets));
+app.get(`${layout.pathname()}pages/first`, handlePage(layout, firstPodlets));
+
+app.get(`${layout.pathname()}pages/second`, handlePage(layout, secondPodlets));
+
+app.get(`${layout.pathname()}pages/third`, handlePage(layout, thirdPodlets));
 
 app.listen(7000, () => console.log("Layout listening on port 7000"));
